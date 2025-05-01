2e9140e606b7849d1a261aea09ddf0c4b989dab3
Alvaro Ruiz Monfillo <alvaroruizmonfillo@gmail.com>
2025-05-01 15:31:43 +0200

    Dia 1 hecho

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,48 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    (this.questions = questions),
+      (this.timeLimit = timeLimit),
+      (this.timeRemaining = timeRemaining),
+      (this.correctAnswers = 0),
+      (this.currentQuestionIndex = 0);
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[randomIndex]] = [
+        this.questions[randomIndex],
+        this.questions[i],
+      ];
+    }
+  }
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (answer) {
+      this.correctAnswers++;
+    }
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 
-    // 2. getQuestion()
+  filterQuestionsByDifficulty(difficulty){
     
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  }
 }
